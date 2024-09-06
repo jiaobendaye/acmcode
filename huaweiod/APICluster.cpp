ccaa3a975b2faf5cedf8c10ce9e60adc2cdf7264
401-ubuntu <240450828@qq.com>
2024-09-06 11:12:10 +0800

    fix include header

--- huaweiod/APICluster.cpp
+++ huaweiod/APICluster.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
 
 using namespace std;
 
--- huaweiod/GrayResume.cpp
+++ huaweiod/GrayResume.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
